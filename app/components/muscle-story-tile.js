1fb558397d7c56fa91d2d5d84a9190e9cd4c17e2
Steve Zaske and Witty Chang <steve+witty@>
2017-08-08 14:19:17 -0700

    Add story details routes and content

--- app/components/muscle-story-tile.js
+++ app/components/muscle-story-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+   goToStory(story) {
+     console.log("go to story sent=",story.id);
+    //  var newRental = this.store.createRecord('rental', params);
+    //  newRental.save();
+    //this.transitionTo('story', story);
+     this.sendAction('goToStory', story);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('musclestory');
+  },
+  actions: {
+   goToStory(story) {
+     this.transitionTo('musclestory', story);
+    }
   }
 });
